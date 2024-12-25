d78e987e041863f0d9296541c9f385fb79425fec
Colby-Frison <colbyfrison100@gmail.com>
2024-12-25 15:32:37 -0600

    File search works

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main() {
+    FILE* file;
+    string name;
+    string fileName = "Users/";
 
-    cout << "Enter name";
+    cout << "Enter Name: ";
+    cin >> name;
+    cout << endl;
 
-    
+    fileName += name;
+    fileName += ".txt";
+
+    file = fopen(fileName.c_str(), "r");
+
+    if (file!=NULL)
+    {
+        // printing the success message
+        cout << "File exists" << endl;
+    }
+    else
+    {
+        // printing the error message
+        cout << "File does not exists" << endl;
+    }
 
     return 0;
 }
