6beaa8901fba481a87bf0aede70b01a4388ca968
Colby-Frison <colbyfrison100@gmail.com>
2024-12-27 22:18:43 -0600

    added files

---
+++ menu.cpp
@@ -0,0 +1,6 @@
+// idea is a main menu that can be used for player creatio and accessing user info
+
+// so I will need to find a way to switch through files/access different games from this main class
+// without hacing to directly close and start different programs
+
+// I could just make all games in one file and hace a selection memu but expirementiaon :)
