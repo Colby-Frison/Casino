66a3ea7f68fc05f5193c15addb15eac735a94ab6
Colby-Frison <colbyfrison100@gmail.com>
2025-01-11 19:27:52 -0600

    start file, can't even really call it an outline

---
+++ roulette.cpp
@@ -0,0 +1,9 @@
+#include "menu.h"
+
+#include <iostream>
+
+using namespace std;
+
+void roulLoop() {
+    
+}
---
+++ slots.cpp
@@ -0,0 +1,10 @@
+#include "menu.h"
+
+#include <iostream>
+
+using namespace std;
+
+void slotLoop(Player player) {
+
+    
+}
